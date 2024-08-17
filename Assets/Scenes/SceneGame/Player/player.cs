ec8e42375719400b6399ebc5469d14a67e69fc57
maru <toshidai.tarou11111@gmail.com>
2024-08-17 21:27:09 +0900

    無敵時間を追加？

---
+++ Assets/Scenes/SceneGame/Player/player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public int hp=20;
+    private float timer=0;
+    private float interval=3;
+    private bool muteki=false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+        if (muteki)
+        {
+            timer += Time.deltaTime;
+            if (interval < timer)
+            {
+                muteki = false;
+                timer = 0;
+            }
+        }
+    }
+
+    public void damage(int damage)
+    {
+        if (!muteki)
+        {
+            hp -= damage;
+
+            muteki = true;
+        }
+
+
+
+    }
+}
