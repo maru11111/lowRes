223f38d9760f76d33b846a5c6ea46c9c4fa520f8
maru <toshidai.tarou11111@gmail.com>
2024-08-17 21:23:20 +0900

    城の当たり判定、hp、hpバーを追加

---
+++ Assets/Scenes/SceneGame/castle/castle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class castle : MonoBehaviour
+{
+
+    public int hp=200;
+    private bool isDefeat=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ゲームオーバー判定
+        if(hp < 0)
+        {
+            isDefeat = true;
+        }
+    }
+
+    public void damage(int damage)
+    {
+        hp -= damage;
+    }
+
+}
---
+++ Assets/Scenes/SceneGame/castle/hpBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpBar : MonoBehaviour
+{
+    Slider slider;
+    int castleHp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = this.GetComponent<Slider>();
+        var castle = GetComponentInParent<castle>();
+        slider.maxValue = castle.hp;
+        Debug.Log("maxValue"+slider.maxValue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("hp" + GetComponentInParent<castle>().hp);
+
+        //hpを取得
+        castleHp = GetComponentInParent<castle>().hp;
+
+        //スライダーを更新
+        slider.value = (float)castleHp;
+    }
+}
