77e39b6622c42c6ff2c843daa63a5086fad359fb
maru <toshidai.tarou11111@gmail.com>
2024-08-24 21:13:21 +0900

    プレイヤーのhpバーを作成

---
+++ Assets/Scenes/SceneGame/UI/playerHpBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHpBar : MonoBehaviour
+{
+    private int maxHp;
+    private int currentHp;
+    private Image hpBar;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpBar = GetComponent<Image>();
+        player = GameObject.Find("Player");
+        updateHp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateHp();
+        hpBar.fillAmount = (float)currentHp / (float)maxHp;
+    }
+
+    private void updateHp()
+    {
+        if (player != null)
+        {
+            maxHp = player.GetComponent<player>().maxHp;
+            currentHp = player.GetComponent<player>().currentHp;
+        }
+        else
+        {
+            Debug.Log("Playerが見つかりませんでした");
+        }
+    }
+}
