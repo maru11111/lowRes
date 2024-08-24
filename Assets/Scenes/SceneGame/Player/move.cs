b85fe0d3b676680e014f59d2c4a2ee6e66200699
maru <toshidai.tarou11111@gmail.com>
2024-08-24 22:07:55 +0900

    走りアニメーションを追加

--- Assets/Scenes/SceneGame/Player/move.cs
+++ Assets/Scenes/SceneGame/Player/move.cs
@@ -11,51 +11,67 @@ public class move : MonoBehaviour
     private float currentSpeed;
     public bool right = false;
     public bool up = false;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!GameObject.Find("Player").GetComponent<takeDamage>().isStop)
+        {
 
         //移動速度を設定
-        if (Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow))
-        {
-            currentSpeed = -speed;
-        }
-        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            currentSpeed = speed;
-        }
-        else
-        {
-            currentSpeed = 0;
-        }
-
-        //向きを変える
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            right = true;
-            this.GetComponent<SpriteRenderer>().flipX = true;
-        }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            right = false;
-            this.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            up = true;
-            //上を向く
-        }
-        else
-        {
-            up = false;
-        }
+            {
+                currentSpeed = -speed;
+                if (!animator.GetBool("Dash"))
+                {
+                    animator.Play("Dash");
+                    animator.SetBool("Dash", true);
+                }
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                currentSpeed = speed;
+                if (!animator.GetBool("Dash"))
+                {
+                    animator.Play("Dash");
+                    animator.SetBool("Dash", true);
+                }
+            }
+            else
+            {
+                currentSpeed = 0;
+                animator.SetBool("Dash", false);
+            }
+
+            //向きを変える
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                right = true;
+                this.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                right = false;
+                this.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                up = true;
+                //上を向く
+            }
+            else
+            {
+                up = false;
+            }
+     }
 
 
 
@@ -63,7 +79,14 @@ public class move : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //移動
-        player.velocity = (new Vector2(currentSpeed, player.velocity.y));
+        if (!GameObject.Find("Player").GetComponent<takeDamage>().isStop)
+        {
+            //移動
+            player.velocity = (new Vector2(currentSpeed, player.velocity.y));
+        }
+        //else
+        //{
+        //    player.velocity = Vector3.zero;
+        //}
     }   
 }
