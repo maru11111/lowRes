e00006e56fc61ff3bd00e83c696e50621c23679a
maru <toshidai.tarou11111@gmail.com>
2024-08-24 22:10:05 +0900

    最大hp、ふっとばし追加、無敵時間変更

--- Assets/Scenes/SceneGame/Player/player.cs
+++ Assets/Scenes/SceneGame/Player/player.cs
@@ -6,21 +6,24 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
 
-    public int hp=20;
+    public int maxHp=200;
+    public int currentHp;
     private float timer=0;
-    private float interval=3;
+    private float interval=1.5f;
     private bool muteki=false;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        currentHp=maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hp <= 0)
+        Debug.Log("currentHp = " + currentHp);
+        if (currentHp <= 0)
         {
             Destroy(this.gameObject);
         }
@@ -37,16 +40,36 @@ public class player : MonoBehaviour
         }
     }
 
-    public void damage(int damage)
+    public void damage(int damage, Vector2 effectPos, GameObject otherObj)
     {
         if (!muteki)
         {
-            hp -= damage;
+            currentHp -= damage;
+
+            //吹っ飛ばし方向
+            int direction;
+            //プレイヤーの右側にぶつかれば
+            if (otherObj.transform.position.x <= this.transform.position.x)
+            {
+                direction = 1;
+            }
+            else
+            {
+                direction = -1;
+            }
+            //吹っ飛ばし
+            GetComponent<takeDamage>().startKnockBack(direction);
+
+            //衝突点にダメージエフェクト
+            GameObject.Find("EffectScript").GetComponent<damageEffect>().damageEffectPlay(effectPos);
+        
+        //マイナスにならないようにする
+        if (currentHp < 0)
+            {
+                currentHp = 0;
+            }
 
             muteki = true;
         }
-
-
-
     }
 }
