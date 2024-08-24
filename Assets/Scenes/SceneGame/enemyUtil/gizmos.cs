acb905790e752f60d774c4cf7713ad465fb1444e
maru <toshidai.tarou11111@gmail.com>
2024-08-24 21:17:12 +0900

    当たり判定の可視化用コンポーネント

---
+++ Assets/Scenes/SceneGame/enemyUtil/gizmos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gizmos : MonoBehaviour
+{
+    // Update is called once per frame
+    void OnDrawGizmos()
+    {
+        Collider2D collider = GetComponent<Collider2D>();
+        if (collider != null)
+        {
+
+            if (collider is BoxCollider2D)
+            {
+                Gizmos.color = Color.red; // 色を指定
+
+                BoxCollider2D box = (BoxCollider2D)collider;
+                Gizmos.DrawWireCube(transform.position, box.bounds.size);
+            }
+            else if (collider is CircleCollider2D)
+            {
+                Gizmos.color = Color.yellow; // 色を指定
+
+                CircleCollider2D circle = (CircleCollider2D)collider;
+                Gizmos.DrawWireSphere(transform.position, circle.radius*2);
+            }
+        }
+    }
+}
