c2805d5d8fb484c140eb3c7b894e8e9e04e14c9c
maru <toshidai.tarou11111@gmail.com>
2024-08-24 21:56:36 +0900

    ダメージエフェクトスクリプト

---
+++ Assets/Scenes/SceneGame/Effect/damageEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class damageEffect : MonoBehaviour
+{
+    public GameObject prefab;
+    public void damageEffectPlay(Vector2 pos)
+    {
+        GameObject explosion = Instantiate(prefab, pos, Quaternion.identity);
+
+        Destroy(explosion, 1f);
+    }
+}
