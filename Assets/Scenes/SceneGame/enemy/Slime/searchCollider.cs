30585459a5040342633be5e52671201ccc6e89dc
maru <toshidai.tarou11111@gmail.com>
2024-08-24 21:26:20 +0900

    スライムの索敵用コライダー

---
+++ Assets/Scenes/SceneGame/enemy/Slime/searchCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class searchCollider : MonoBehaviour
+{
+    public slime slime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+       slime.setAttackTarget(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        slime.removeAttackTarget(collision.gameObject);
+    }
+}
