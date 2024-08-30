af1f8af441cbb0909e8cd051249cccf5275e8be1
maru <toshidai.tarou11111@gmail.com>
2024-08-30 23:46:38 +0900

    索敵コライダー

---
+++ Assets/Scenes/SceneGame/enemy/EnemyUtil/searchColliderForAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class searchColliderForAttack : MonoBehaviour
+{
+
+    public baseEnemy baseEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (baseEnemy.target != null)
+        {
+            //当たった物体がターゲットかどうか
+            if (collision.gameObject == baseEnemy.target)
+            {
+                baseEnemy.withinAttackRange = true;
+            }
+        }
+
+    }
+        private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (baseEnemy.target != null)
+        {
+            //当たった物体がターゲットかどうか
+            if (collision.gameObject == baseEnemy.target)
+            {
+                baseEnemy.withinAttackRange = false;
+            }
+        }
+    }
+}
