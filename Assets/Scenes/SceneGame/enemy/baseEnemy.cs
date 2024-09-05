e1818418e27d2f5d84dba048cdf83486873ceac6
maru <toshidai.tarou11111@gmail.com>
2024-09-06 00:32:31 +0900

    汎用性向上

--- Assets/Scenes/SceneGame/enemy/baseEnemy.cs
+++ Assets/Scenes/SceneGame/enemy/baseEnemy.cs
@@ -6,27 +6,70 @@ using UnityEngine;
 public class baseEnemy : MonoBehaviour
 {
 
-    protected int hp;
+    public int maxHp;
+    public int currentHp;
     protected int power;
     protected int powerToCastle;
     protected float attackInterval;
+    protected float walkInterval;
     protected float timer=0;
+    protected float walkTimer;
     public bool? isEnemy=null;
     protected bool moveAttack=false;
+    public bool withinAttackRange = false;
     protected bool attacking = false;
+    public GameObject target;
+    public bool flagOnFloor = false;
+    public damageEffect damageEffect;
+    protected castle playerCastleScript;
+    protected castle enemyCastleScript;
+    protected GameObject playerCastle;
+    protected GameObject enemyCastle;
+    public player player;
+    public Rigidbody2D rigid;
+    public Animator anim;
+
+    public static int enemyNum=0;
+
     // Start is called before the first frame update
     virtual protected void Start()
     {
+        //敵と眷属の数カウント
+        enemyNum += 1;
+        Debug.Log("敵と眷属の数の合計 : " + enemyNum);
+
+        damageEffect = GameObject.Find("EffectScript").GetComponent<damageEffect>();
+       
+        if (isEnemy.Value){
+            playerCastleScript = GameObject.Find("PlayerCastle").GetComponent<castle>();
+        }
+        else
+        {
+            enemyCastleScript = GameObject.Find("EnemyCastle").GetComponent<castle>();
+        }
+        player = GameObject.Find("Player").GetComponent<player>();
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+
+        currentHp = maxHp;
+
+        //はじめは即時攻撃できるように
+        timer = attackInterval;
+
         if (isEnemy.Value)
         {
-
+            var canvas = transform.Find("Canvas");
+            if (canvas != null)
+            {
+                //hpバーを非アクティブに
+                canvas.gameObject.SetActive(false);
+            }
+            playerCastle = GameObject.Find("PlayerCastle");
         }
         //眷属の場合
         else if(!isEnemy.Value)
         {
-            //タグとレイヤーを変更
-            this.gameObject.layer = LayerMask.NameToLayer("Friend");
-            this.gameObject.tag = "Friend";
+            enemyCastle = GameObject.Find("EnemyCastle");
         }
         else
         {
@@ -34,18 +77,48 @@ public class baseEnemy : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        enemyNum -= 1;
+        Debug.Log("敵と眷属の数の合計 : " + enemyNum);
+
+    }
+
     // Update is called once per frame
     virtual protected void Update()
     {
         defeat();
         move();
-        //attack();
+    }
+
+    public void targetNullCheck()
+    {
+        if (target == null)
+        {
+            if (isEnemy.Value)
+            {
+                target = playerCastle;
+            }
+            else if (!isEnemy.Value)
+            {
+                target = enemyCastle;
+            }
+        }
     }
 
     //ダメージを受けるためのメソッド
-    public void damage(int damage)
+    virtual public void damage(int damage, Vector2 effectPos)
     {
-        hp -= damage;
+        currentHp -= damage;
+        //衝突点にダメージエフェクト
+        damageEffect.damageEffectPlay(effectPos);
+    }
+    //ダメージを受けるためのメソッド
+    virtual public void damage(int damage, Vector2 effectPos, GameObject obj)
+    {
+        currentHp -= damage;
+        //衝突点にダメージエフェクト
+        damageEffect.damageEffectPlay(effectPos);
     }
 
     //死亡するためのメソッド
@@ -57,7 +130,7 @@ public class baseEnemy : MonoBehaviour
     //死亡処理のメソッド
     public void defeat()
     {
-        if (hp <= 0)
+        if (currentHp <= 0)
         {
             destroy();
         }
@@ -73,53 +146,207 @@ public class baseEnemy : MonoBehaviour
     }
 
     //攻撃するためのメソッド
-    virtual public void attack(GameObject obj)
+    virtual public void attack(GameObject obj, Vector2 effectPos)
     {
-        if (!isEnemy.Value)
-        {
-
-        }
-    }
-
-    //自身に当たった弾の削除処理
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+        //自身が敵であれば
         if (isEnemy.Value)
         {
-            //弾に当たれば弾のdestroy処理
-            if (collision.gameObject.tag == "Bullet")
+            //プレイヤー城にダメージ
+            if (obj.CompareTag("PlayerCastle"))
             {
-                collision.GetComponent<normalBullet>().destroy();
+                playerCastleScript.damage(power, effectPos);
+            }
+            //プレイヤーにダメージ
+            if (obj.CompareTag("Player"))
+            {
+                player.damage(power, effectPos, this.gameObject);
+            }
+            //眷属にダメージ
+            if (obj.CompareTag("Friend") || obj.CompareTag("FriendTank"))
+            {
+                var baseEnemy = obj.GetComponent<baseEnemy>();
+                if (baseEnemy != null)
+                {
+                    Debug.Log(obj + "に" + power + "damage");
+                    baseEnemy.damage(power, effectPos);
+                }
+            }
+        }
+        //自身が眷属であれば
+        else if (!isEnemy.Value)
+        {
+            //敵城にダメージ
+            if (obj.CompareTag("EnemyCastle"))
+            {
+                enemyCastleScript.damage(power, effectPos);
+            }
+            //敵にダメージ
+            if (obj.CompareTag("Enemy") || obj.CompareTag("EnemyTank"))
+            {
+                var baseEnemy = obj.GetComponent<baseEnemy>();
+                if (baseEnemy != null)
+                {
+                    baseEnemy.damage(power, effectPos);
+                }
             }
         }
     }
 
+    //ターゲットを決める(Stayで使用
+    public void setAttackTarget(GameObject col)
+    {
+        //敵の場合
+        if (isEnemy.Value)
+        {
+            //targetがnull(以前のターゲットが死亡後)または城の場合
+            if (target == null || target.CompareTag("PlayerCastle"))
+            {
+                //タンクかプレイヤーならターゲット
+                if (col.CompareTag("FriendTank") || col.CompareTag("Player"))
+                {
+                    target = col;
+                }
+                //付近の眷属をターゲット
+                else if (col.CompareTag("Friend"))
+                {
+                    target = col;
+                }
+            }
+            //ターゲット変更
+            else
+            {
+                //ターゲットがタンクでない(プレイヤーの)とき
+                if (!target.CompareTag("FriendTank"))
+                {
+                    //タンクを優先してターゲット
+                    if (col.CompareTag("FriendTank"))
+                    {
+                        target = col;
+                    }
+                }
+            }
+        }
+        //眷属の場合
+        else
+        {
+            //targetがnull(以前のターゲットが死亡後)または城の場合
+            if (target == null || target.CompareTag("EnemyCastle"))
+            {
+                //タンクを優先してターゲット
+                if (col.CompareTag("EnemyTank"))
+                {
+                    target = col;
+                }
+                //付近の敵をターゲット
+                else if (col.CompareTag("Enemy"))
+                {
+                    target = col;
+                }
+            }
+            //ターゲット変更
+            else
+            {
+                //ターゲットがタンクでないとき
+                if (!target.CompareTag("EnemyTank"))
+                {
+                    //タンクを優先してターゲット
+                    if (col.CompareTag("EnemyTank"))
+                    {
+                        target = col;
+                    }
+                }
+            }
+        }
+    }
+
+    //ターゲットを外す(Exitで使用
+    public void removeAttackTarget(GameObject col)
+    {
+        //ターゲットがnullでないことが前提
+        if (target != null)
+        {
+            //敵の場合
+            if (isEnemy.Value)
+            {
+                if (col.CompareTag("Player"))
+                {
+                    target = playerCastle;
+                }
+            }
+            //眷属の場合
+            else
+            {
+                //ターゲットした敵を追いかけ続ける
+            }
+        }
+    }
+
+    virtual protected void setDirection()
+    {
+        //進行方向を決める
+        //ターゲットがnullでないことが前提
+        if (target != null)
+        {
+            //ターゲットが右にいたら
+            if (transform.position.x <= target.transform.position.x)
+            {
+                //右を向く
+                if (this.transform.localScale.x < 0)
+                {
+                    this.transform.localScale = new Vector3(-transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+                }
+            }
+            else
+            {
+                //左を向く
+                if (0 < this.transform.localScale.x)
+                {
+                    this.transform.localScale = new Vector3(-transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+                }
+            }
+        }
+    }
+
+    //アニメーション用
+    //BodyTypeを変える
+    private void changeKinematic()
+    {
+        rigid.bodyType = RigidbodyType2D.Kinematic;
+    }
+    private void changeDynamic()
+    {
+        rigid.bodyType = RigidbodyType2D.Dynamic;
+    }
+
     //自身に当たったオブジェクトのダメージ処理
-    virtual protected void OnCollisionEnter2D(Collision2D collision)
+    virtual protected void OnTriggerStay2D(Collider2D collision)
     {
         //自分が敵であれば
         if (isEnemy.Value)
         {
             //プレイヤーにダメージ
-            if (collision.gameObject.tag == "Player")
+            if (collision.gameObject.CompareTag("Player"))
             {
-                collision.gameObject.GetComponent<player>().damage(power);
-            }
-
-            //眷属にダメージ
-            if (collision.gameObject.tag == "Friend")
-            {
-                collision.gameObject.GetComponent<baseEnemy>().damage(power);
+                Vector2 collisionPos = collision.ClosestPoint(this.transform.position);
+                //ダメージ処理
+                player.damage(power, collisionPos, this.gameObject);
             }
         }
-        //自分が眷属であれば
-        else if(!isEnemy.Value)
+
+
+        //床にいるか
+        if (collision.gameObject.CompareTag("Stage"))
         {
-            //敵にダメージ
-            if (collision.gameObject.tag == "Enemy")
-            {
-                collision.gameObject.GetComponent<baseEnemy>().damage(power);
-            }
+            flagOnFloor = true;
+        }
+    }
+
+    //床にいるか
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Stage"))
+        {
+            flagOnFloor = false;
         }
     }
 }
