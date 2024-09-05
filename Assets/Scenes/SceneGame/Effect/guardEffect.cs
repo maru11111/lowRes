7d0d0ed3b81f76327718317524334961d1a4348b
maru <toshidai.tarou11111@gmail.com>
2024-09-06 00:41:22 +0900

    ガード音追加

---
+++ Assets/Scenes/SceneGame/Effect/guardEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class guardEffect : MonoBehaviour
+{
+    public GameObject prefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void guardEffectPlay(Vector2 pos)
+    {
+        GameObject guard = Instantiate(prefab, pos, Quaternion.identity);
+
+        Destroy(guard, 1f);
+    }
+
+}
