72b7093044e407aaf5684ef27cedc69f338987b6
maru <toshidai.tarou11111@gmail.com>
2024-09-06 00:41:47 +0900

    魔法陣作成

---
+++ Assets/Scenes/SceneGame/Effect/magicCircle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magicCircle : MonoBehaviour
+{
+    public Animator anim;
+    private float timer=0;
+    private float diappearAnimationTime=2;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (diappearAnimationTime < timer)
+        {
+            anim.SetBool("DisAppear",true);
+        }
+    }
+}
---
+++ Assets/Scenes/SceneGame/Effect/playMagicCircle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMagicCircle : MonoBehaviour
+{
+    public GameObject prefab;
+
+
+    public void MagicCirclePlay(Vector2 pos)
+    {
+        GameObject magicCircle = Instantiate(prefab, pos, Quaternion.identity);
+
+        Destroy(magicCircle, 3f);
+    }
+}
