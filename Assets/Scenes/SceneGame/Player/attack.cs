826cd932d21db422aa18956451039f02d360a8a6
maru <toshidai.tarou11111@gmail.com>
2024-09-06 00:37:37 +0900

    キー変更、ポーズ対応

--- Assets/Scenes/SceneGame/Player/attack.cs
+++ Assets/Scenes/SceneGame/Player/attack.cs
@@ -6,19 +6,21 @@ public class attack : MonoBehaviour
 {
 
     [SerializeField] GameObject prefab;
+    public rolling rollingScript;
+    public pause pauseScript;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!GameObject.Find("Player"). GetComponent<takeDamage>().isStop)
+        if (!GameObject.Find("Player"). GetComponent<takeDamage>().isStop && !rollingScript.isRolling && !pauseScript.isPause)
         {
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (Input.GetKeyDown(KeyCode.S))
             {
                 Instantiate(prefab, GameObject.Find("Player").transform.position, Quaternion.identity);
             }
