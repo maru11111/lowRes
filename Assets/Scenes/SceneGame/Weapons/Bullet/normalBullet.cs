a1001535fc479f3a8d4d799d1ac368034afc186c
maru <toshidai.tarou11111@gmail.com>
2024-09-20 11:13:30 +0900

    ショップ補正追加、グラフィックを追加、射程を調整

--- Assets/Scenes/SceneGame/Weapons/Bullet/normalBullet.cs
+++ Assets/Scenes/SceneGame/Weapons/Bullet/normalBullet.cs
@@ -17,7 +17,10 @@ public class normalBullet : MonoBehaviour
     //(左なら-1右なら1上なら0, 上なら1それ以外は0)
     protected Vector2 direction = new (1,0);
     private float speed=10;
-    private int power=10;
+    private int power=0;
+    private int powerLevel1 = 10;
+    private int powerLevel2 = 8;
+    private int powerLevel3 = 200;
     private bool isShot=false;
     private float chargeTimer=0;
     private float chargeTime2nd=1.5f;
@@ -25,14 +28,54 @@ public class normalBullet : MonoBehaviour
     private Level level = Level.first;
     private bool flagDestroyOnce=false;
     private bool isPenetrate;
+    public static int bulletInstanceNum=0;
+    public static int bulletInstanceMaxNum=3;
+
+    public Animator anim;
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] images;
 
     // Start is called before the first frame update
     void Start()
     {
+        
+        //プレイヤー攻撃力レベル補正
+        if(SaveDataManager.data.playerPowerLevel == 0)
+        {
+            //変化なし
+        }
+        else if(SaveDataManager.data.playerPowerLevel == 1)
+        {
+            powerLevel1 += 2;
+            powerLevel2 += 8;
+            powerLevel3 += 50;
+        }
+        else if(SaveDataManager.data.playerPowerLevel == 2)
+        {
+            powerLevel1 += 3;
+            powerLevel2 += 16;
+            powerLevel3 += 100;
+        }
+        else if(SaveDataManager.data.playerPowerLevel == 3)
+        {
+            powerLevel1 += 6;
+            powerLevel2 += 24;
+            powerLevel3 += 150;
+        }
+
         //打つ前は当たり判定オフに
         this.GetComponent<CircleCollider2D>().enabled = false;
         //弾の向きをプレイヤーが向いている方向に
         changeDir();
+        //弾のインスタンス数を増やす
+        bulletInstanceNum++;
+    }
+
+    private void OnDestroy()
+    {
+        //弾のインスタンス数を減らす
+        bulletInstanceNum--;
     }
     void Update()
     {
@@ -42,12 +85,12 @@ public class normalBullet : MonoBehaviour
             isShot = true;
         }
         //発射前
-        if(!isShot)
+        if (!isShot)
         {
             //ダメージを受けたら攻撃解除
             if (GameObject.Find("Player").GetComponent<takeDamage>().isStop)
             {
-                destroy();  
+                destroy();
             }
 
             //レベル判定
@@ -56,35 +99,42 @@ public class normalBullet : MonoBehaviour
             {
                 level = Level.third;
             }
-            else if(chargeTime2nd < chargeTimer)
+            else if (chargeTime2nd < chargeTimer)
             {
                 level = Level.second;
             }
 
             //レベル別処理
-            if (level == Level.first)
+            switch (level)
             {
-                speed = 10f;
-                power = 10;
-                isPenetrate = false;
-                gameObject.tag = "Untagged";
-            }
-            else if(level == Level.second)
-            {
-                power = 50;
-                speed = 25f;
-                isPenetrate = true;
-                gameObject.tag = "PiercingAttack";
-                this.transform.localScale = new(1.05f, 1.05f, 1);
-            }
-            else if(level == Level.third) 
-            {
-                power = 200;
-                speed = 8f;
-                isPenetrate = false;
-                gameObject.tag = "Untagged";
-                
-                this.transform.localScale = new(1.3f, 1.3f, 1);
+                case Level.first:
+                    speed = 10f;
+                    power = powerLevel1;
+                    isPenetrate = false;
+                    gameObject.tag = "Untagged";
+                    spriteRenderer.sprite = images[0];
+                  
+                    break;
+
+                case Level.second:
+                    power = powerLevel2;
+                    speed = 25f;
+                    isPenetrate = true;
+                    gameObject.tag = "PiercingAttack";
+                    this.transform.localScale = new(1.05f, 1.05f, 1);
+                    spriteRenderer.sprite = images[1];
+                   
+                    break;
+
+                case Level.third:
+                    power = powerLevel3;
+                    speed = 8f;
+                    isPenetrate = false;
+                    gameObject.tag = "Untagged";
+                    this.transform.localScale = new(1.3f, 1.3f, 1);
+                    spriteRenderer.sprite = images[2];
+                  
+                    break;
             }
             changeDir();
             move();
@@ -95,7 +145,20 @@ public class normalBullet : MonoBehaviour
         {
             if (!flagDestroyOnce)
             {
-                Destroy(this.gameObject, 2);
+                switch (level)
+                {
+                    case Level.first:
+                        Destroy(this.gameObject, 0.375f);
+                        break;
+
+                    case Level.second:
+                        Destroy(this.gameObject, 2);
+                        break;
+                    
+                    case Level.third:
+                        Destroy(this.gameObject, 2);
+                        break;
+                }
                 flagDestroyOnce = true;
                 this.GetComponent<CircleCollider2D>().enabled = true;
             }
@@ -113,10 +176,35 @@ public class normalBullet : MonoBehaviour
         if (GameObject.Find("Player").GetComponent<move>().right)
         {
             direction = new(1, 0);
+            switch (level)
+            {
+                case Level.first:
+                    transform.Rotate(0, 0, -90 * Time.deltaTime);
+                    break;
+                case Level.second:
+                    transform.Rotate(0, 0, -480*2 * Time.deltaTime);
+                    break;
+                case Level.third:
+                    transform.Rotate(0, 0, -15 * Time.deltaTime);
+                    break;
+            }
         }
         else
         {
             direction = new(-1, 0);
+            transform.Rotate(0, 0, 15 * Time.deltaTime);
+            switch (level)
+            {
+                case Level.first:
+                    transform.Rotate(0, 0, 90 * Time.deltaTime);
+                    break;
+                case Level.second:
+                    transform.Rotate(0, 0, 480*2 * Time.deltaTime);
+                    break;
+                case Level.third:
+                    transform.Rotate(0, 0, 15 * Time.deltaTime);
+                    break;
+            }
         }
         //if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         //{
@@ -190,10 +278,9 @@ public class normalBullet : MonoBehaviour
                 Vector2 collisionPos = collision.ClosestPoint(transform.position);
 
                 castle.damage(power, collisionPos);
+                //自身を削除
+                destroy();
             }
-
-            //自身を削除
-            destroy();
         }
 
     }
