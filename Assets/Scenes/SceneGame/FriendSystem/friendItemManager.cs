afafe38421d74d75a2104654c827ced2d3e7c68a
maru <toshidai.tarou11111@gmail.com>
2024-09-20 10:42:04 +0900

    updateを削除

--- Assets/Scenes/SceneGame/FriendSystem/friendItemManager.cs
+++ Assets/Scenes/SceneGame/FriendSystem/friendItemManager.cs
@@ -23,10 +23,4 @@ public class friendItemManager : MonoBehaviour
         //格納失敗
         return false;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
