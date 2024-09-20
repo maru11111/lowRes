cdc93b512e28e9b1a1ecf85500f97a8ebc89364a
maru <toshidai.tarou11111@gmail.com>
2024-09-20 19:34:16 +0900

    城が破壊された時のエフェクトを再生するためのスクリプト

---
+++ Assets/Scenes/SceneGame/Effect/castleBrokenEffectPlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class castleBrokenEffectPlay : MonoBehaviour
+{
+    public castleBrokenEffect castleBrokenEffect;
+
+    public void play(Vector2 pos)
+    {
+        Instantiate(castleBrokenEffect, pos, Quaternion.identity);
+    }
+}
