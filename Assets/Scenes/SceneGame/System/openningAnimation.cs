6de5fc4ff0d90ce4970192e08d811e97b4d0e9ef
maru <toshidai.tarou11111@gmail.com>
2024-09-20 19:35:45 +0900

    シーン遷移時のアニメーション

---
+++ Assets/Scenes/SceneGame/System/openningAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class openningAnimation : MonoBehaviour
+{    
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        StartCoroutine(playAnimation());
+
+    }
+
+    IEnumerator playAnimation()
+    {
+        yield return new WaitForSecondsRealtime(0.2f);
+
+        this.transform.DOMoveX(-1280 / 2, 0.25f).SetUpdate(true);
+        yield return new WaitForSecondsRealtime(0.25f);
+        Time.timeScale = 1;
+    }
+
+}
