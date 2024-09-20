f06c691623e6a0f2db80d2331aa5ceee75341b40
maru <toshidai.tarou11111@gmail.com>
2024-09-20 19:42:13 +0900

    眷属のアイテム欄でのHP表示

---
+++ Assets/Scenes/SceneGame/FriendSystem/friendHpBarForItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class friendHpBarForItem : MonoBehaviour
+{
+    public friendItemBox friendItemBox;
+    private int maxHp;
+    private int currentHp;
+    private Image hpBar;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpBar = GetComponent<Image>();
+        updateHp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateHp();
+        hpBar.fillAmount = (float)currentHp / (float)maxHp;
+    }
+    private void updateHp()
+    {
+        if (friendItemBox.baseEnemyScript != null)
+        {
+            maxHp = friendItemBox.baseEnemyScript.maxHp;
+            currentHp = friendItemBox.baseEnemyScript.currentHp;
+        }
+        else
+        {
+            Debug.Log("baseEnemyが見つかりませんでした");
+        }
+    }
+}
