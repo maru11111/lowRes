91a1d9a3cd60db62406a97bba4bae2db57d54cf1
maru <toshidai.tarou11111@gmail.com>
2024-09-20 19:27:46 +0900

    タイトルシーンの注意画面のためのスクリプト

---
+++ Assets/Scenes/SceneTitle/CautionManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CautionManager : MonoBehaviour
+{
+    public GameObject cautionCanvas;
+
+    public TextMeshProUGUI titleText;
+
+    public SaveDataManager saveDataManager;
+
+    public GameObject yesObj;
+    public GameObject noObj;
+    public GameObject startObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void startOn()
+    {
+        SceneManager.LoadScene("SceneHome");
+    }
+
+    public void cautionOn()
+    {
+        cautionCanvas.SetActive(true);
+        titleText.color = new Color(100f/255f, 100f / 255f, 100f / 255f);
+        EventSystem.current.SetSelectedGameObject(noObj);
+    }
+    public void cautionOff()
+    {
+        cautionCanvas.SetActive(false);
+        titleText.color = new Color(1, 1, 1);
+        EventSystem.current.SetSelectedGameObject(startObj);
+    }
+
+    public void yesPressed()
+    {
+        cautionOff();
+        saveDataManager.ResetSaveData();
+    }
+    public void noPressed()
+    {
+        cautionOff();
+    }
+}
