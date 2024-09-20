1b22a4983e36b61c3d97baef4d5d14cded5e9064
maru <toshidai.tarou11111@gmail.com>
2024-09-20 19:32:50 +0900

    セーブデータ

---
+++ Assets/Scenes/SaveData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class SaveData
+{
+    public int playerPoint;
+    public int playerHPLevel;
+    public int playerPowerLevel;
+    public int castleHPLevel;
+    public int friendDropLevel;
+    public int friendStrengthenLevel;
+    //0ならオフ, 1ならオン
+    public int onDoubleJump;
+    public int onRolling;
+    public int stage1HighScore;
+    public int stage2HighScore;
+    public int stage3HighScore;
+    //0で未クリア, 1でクリア
+    public int isStage1Clear;
+    public int isStage2Clear;
+    public int isStage1PerfectClear;
+    public int isStage2PerfectClear;
+    public int isStage3PerfectClear;
+    //0で演出前, 1で演出後
+    public int isStage1PerfectClearFirstFlag;
+    public int isStage2PerfectClearFirstFlag;
+    public int isStage3PerfectClearFirstFlag;
+}
