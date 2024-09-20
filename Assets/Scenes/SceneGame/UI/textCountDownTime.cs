487cd53c272a2bc887fe6d1ade8dc9c6e6b9a946
maru <toshidai.tarou11111@gmail.com>
2024-09-20 19:39:39 +0900

    残り時間の表示

---
+++ Assets/Scenes/SceneGame/UI/textCountDownTime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class textCountDownTime : MonoBehaviour
+{
+    private float timer=0;
+    private float deadWaitTime=11;
+    public TextMeshProUGUI text;
+    public bool isWaitEnd=false;
+
+    public void resetTimer()
+    {
+        timer = 0;
+        isWaitEnd = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        text.text = ((int)(deadWaitTime - timer)).ToString();
+
+        if (deadWaitTime < timer)
+        {
+            isWaitEnd = true;
+        }
+    }
+}
